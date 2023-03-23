3fe1d228040170e80ac34e542faca26d398626ea
Oskar Svanström <65446534+Osksacho@users.noreply.github.com>
2023-03-23 11:50:02 +0100

    Update view.py

--- views/view.py
+++ views/view.py
@@ -1,8 +1,6 @@
-def show_message(name1, name2):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}. Welcome to the gitStream world')  # Press Ctrl+8 to toggle the breakpoint.
+def run_view(name):
+    print(f'View, {name} is showing!')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Demo!')
+    run_view('Demo!')
